1d798df4a6f3a5ff727c3d57c554f4a56b0dab70
xianhao.qing <xianhao.qing@ck-telecom.com>
2015-05-04 14:56:43 +0800

    can not update

--- graber/models.py
+++ graber/models.py
@@ -32,6 +32,7 @@ class Employee(models.Model):
 class Community(models.Model):
     name = models.CharField(max_length=200)
     review_base = models.CharField(max_length=2048)
+    disabled = models.BooleanField(default=False)
     def __str__(self):
         return '%s' % self.name
 
